dcd2c7df94bd6b1c40c531a87a6d4e9ebb95a923
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-10 16:11:58 +0800

    add:chassis基类函数实现

---
+++ base/chassis/chassis.cpp
@@ -0,0 +1,23 @@
+//
+// Created by 98383 on 24-10-8.
+//
+
+#include "chassis.h"
+
+void Chassis::SetVx(float vx_) { // 考虑加低通滤波
+    ref_.vx = vx_;
+}
+
+void Chassis::SetVy(float vy_) {
+    ref_.vy = vy_;
+}
+
+void Chassis::SetWz(float wz_) {
+    ref_.wz = wz_;
+}
+
+void Chassis::SetSpeed(float vx_, float vy_, float wz_) {
+    SetVx(vx_);
+    SetVy(vy_);
+    SetWz(wz_);
+}
