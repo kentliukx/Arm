eee3aa15a5b05e07da424ba6200530a61845092c
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-12 09:25:17 +0800

    add:板载蜂鸣器相关类及函数

--- base/bsp_buzzer/bsp_buzzer.cpp
+++ base/bsp_buzzer/bsp_buzzer.cpp
@@ -1,5 +1,58 @@
-//
-// Created by 98383 on 24-10-12.
-//
+/**
+******************************************************************************
+* @file    bsp_buzzer.cpp/h
+* @brief   Buzzer 板载蜂鸣器
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
 
 #include "bsp_buzzer.h"
+#include <string.h>
+#include "cmsis_os.h"
+
+#ifdef DBC
+TIM_HandleTypeDef* board_buzzer_tim = &htim4;
+const uint32_t board_buzzer_channel = TIM_CHANNEL_3;
+#elif defined DBA
+TIM_HandleTypeDef* board_buzzer_tim = &htim12;
+const uint32_t board_buzzer_channel = TIM_CHANNEL_1;
+#endif
+
+#if (defined DBC) or (defined DBA)
+
+void buzzerDelay(uint16_t ms) {
+   // HAL_Delay(ms);
+   osDelay(ms);
+}
+
+BoardBuzzer::BoardBuzzer(void)
+   : htim_(board_buzzer_tim), ch_(board_buzzer_channel) {}
+
+void BoardBuzzer::init(void) {
+   HAL_TIM_PWM_Start(htim_, ch_);
+}
+
+void BoardBuzzer::playNote(MusicNote_t note) {
+   note_ = note;
+   stop_tick_ = HAL_GetTick() + note.time;
+   if (note_.note == 0) {
+       __HAL_TIM_SET_AUTORELOAD(htim_, 0);
+   } else {
+       __HAL_TIM_SET_AUTORELOAD(htim_, 1e6 / note_.note);
+       __HAL_TIM_SET_COMPARE(htim_, ch_, 1e5 / note_.note);
+   }
+   while (HAL_GetTick() < stop_tick_) {
+       buzzerDelay(10);
+   }
+}
+
+void BoardBuzzer::playMusic(const MusicNote_t* notes, uint16_t len) {
+   for (int i = 0; i < len; i++) {
+       memcpy(&note_, (MusicNote_t*)notes + i, sizeof(MusicNote_t));
+       playNote(note_);
+   }
+}
+
+#endif
--- base/bsp_buzzer/bsp_buzzer.h
+++ base/bsp_buzzer/bsp_buzzer.h
@@ -1,8 +1,86 @@
-//
-// Created by 98383 on 24-10-12.
-//
+/**
+******************************************************************************
+* @file    bsp_buzzer.cpp/h
+* @brief   Buzzer 板载蜂鸣器
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
 
-#ifndef RM_FRAME_BSP_BUZZER_H
-#define RM_FRAME_BSP_BUZZER_H
+#ifndef BSP_BUZZER_H
+#define BSP_BUZZER_H
 
-#endif//RM_FRAME_BSP_BUZZER_H
+#include "hardware_config.h"
+#include "tim.h"
+
+#define L1 262
+#define L1U 277
+#define L2 294
+#define L2U 311
+#define L3 330
+#define L4 349
+#define L4U 370
+#define L5 392
+#define L5U 415
+#define L6 440
+#define L6U 466
+#define L7 494
+
+#define M1 523
+#define M1U 554
+#define M2 587
+#define M2U 622
+#define M3 659
+#define M4 698
+#define M4U 740
+#define M5 784
+#define M5U 831
+#define M6 880
+#define M6U 932
+#define M7 988
+
+#define H1 1046
+#define H1U 1109
+#define H2 1175
+#define H2U 1245
+#define H3 1318
+#define H4 1397
+#define H4U 1480
+#define H5 1568
+#define H5U 1661
+#define H6 1760
+#define H6U 1865
+#define H7 1976
+
+typedef struct MusicNote {
+   uint16_t note;
+   uint32_t time;
+} MusicNote_t;
+
+// music notes of Super Mario
+const MusicNote_t music_super_mario[] = {
+       {H3, 100}, {0, 50}, {H3, 250}, {0, 50},   {H3, 100}, {0, 50}, {0, 150},
+       {H1, 100}, {0, 50}, {H3, 250}, {0, 50},   {H5, 250}, {0, 50}, {0, 300},
+       {M5, 250}, {0, 50}, {0, 200},  {H1, 300}, {0, 50}};
+
+#if (defined DBC) or (defined DBA)
+
+class BoardBuzzer {
+public:
+   BoardBuzzer(void);
+
+   void init(void);
+   void playNote(MusicNote_t note);
+   void playMusic(const MusicNote_t* notes, uint16_t len);
+
+private:
+   TIM_HandleTypeDef* htim_;
+   uint32_t ch_;
+   MusicNote_t note_;
+   uint32_t stop_tick_;
+};
+
+#endif
+
+#endif  // BSP_BUZZER_H
