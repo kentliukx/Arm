0b3179ae63187af71bb78745654254bc728056d3
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-12 09:34:32 +0800

    add:connect类添加

--- common/connect/connect.cpp
+++ common/connect/connect.cpp
@@ -1,5 +1,52 @@
-//
-// Created by 98383 on 24-10-12.
-//
+/**
+******************************************************************************
+* @file    connect.cpp/h
+* @brief   Connect state handle. 连接状态处理
+* @author  Spoon Guan
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
 
 #include "connect.h"
+#include "main.h"
+
+// Check connect status. Return true-connected, false-unconnected
+// 检测连接状态
+bool Connect::check(void) {
+   if (HAL_GetTick() - last_tick_ > timeout_) {
+       if (flag_) {
+           edge_ = DISCONNECT;
+           freq_ = 0;
+       } else {
+           edge_ = UNCHANGED;
+       }
+       flag_ = false;
+   } else {
+       if (flag_) {
+           edge_ = UNCHANGED;
+       }
+   }
+   return flag_;
+}
+
+// Refresh connect status(Commonly called in rx callback).
+// Return true-unconnected->connected, false-keep connected
+// 刷新连接状态(一般在接收回调函数调用)
+// 返回 true-未连接->连接，false-保持连接
+ConnectEdge_e Connect::refresh(void) {
+   if (!flag_) {
+       edge_ = CONNECT;
+       flag_ = true;
+   } else {
+       edge_ = UNCHANGED;
+   }
+   if (HAL_GetTick() - last_tick_ < 1) {
+       freq_ = 1e3f;
+   } else {
+       freq_ = 1e3f / (HAL_GetTick() - last_tick_);
+   }
+   last_tick_ = HAL_GetTick();
+   return edge_;
+}
--- common/connect/connect.h
+++ common/connect/connect.h
@@ -1,8 +1,55 @@
-//
-// Created by 98383 on 24-10-12.
-//
+/**
+******************************************************************************
+* @file    connect.cpp/h
+* @brief   Connect state handle. 连接状态处理
+* @author  Spoon Guan
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
 
-#ifndef RM_FRAME_CONNECT_H
-#define RM_FRAME_CONNECT_H
+#ifndef CONNECT_H
+#define CONNECT_H
 
-#endif//RM_FRAME_CONNECT_H
+#include <stdint.h>
+
+// Connect edge(change) judgement 连接状态变化边沿
+typedef enum ConnectEdge {
+   UNCHANGED,   // unchanged
+   CONNECT,     // unconnect->connect
+   DISCONNECT,  // connect->unconnect
+} ConnectEdge_e;
+
+// Connect class 连接类
+class Connect {
+public:
+   Connect(uint32_t timeout)
+       : flag_(false), last_tick_(0), timeout_(timeout), freq_(0) {}
+
+   // Check connect status. Return true-connected, false-unconnected
+   // 检测连接状态
+   bool check(void);
+
+   // Refresh connect status(Commonly called in rx callback).
+   // Return connet edge(state change)
+   // 刷新连接状态(一般在接收回调函数调用)，返回连接状态的变化
+   ConnectEdge_e refresh(void);
+
+   // Last refresh tick
+   // 最后一次刷新时间
+   uint32_t lastTick(void) { return last_tick_; }
+
+   // connect frequency
+   // 连接频率
+   float freq(void) { return freq_; }
+
+private:
+   bool flag_;           // connect state flag. true-connected, false-unconnected
+   ConnectEdge_e edge_;  // connect edge
+   uint32_t last_tick_;  // last update tick(system clock, ms)
+   uint32_t timeout_;    // t>timeout, flag=false
+   float freq_;          // frequency(Hz)
+};
+
+#endif  // CONNECT_H
