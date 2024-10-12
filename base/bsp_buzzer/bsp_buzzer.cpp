31a793ef81b3569312991721a46ff7fe7da844cc
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-12 09:23:45 +0800

    addfile:添加板载蜂鸣器模块

---
+++ base/bsp_buzzer/bsp_buzzer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 98383 on 24-10-12.
+//
+
+#include "bsp_buzzer.h"
---
+++ base/bsp_buzzer/bsp_buzzer.h
@@ -0,0 +1,8 @@
+//
+// Created by 98383 on 24-10-12.
+//
+
+#ifndef RM_FRAME_BSP_BUZZER_H
+#define RM_FRAME_BSP_BUZZER_H
+
+#endif//RM_FRAME_BSP_BUZZER_H
