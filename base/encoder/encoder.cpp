42f6a62494d6ae41495009dbe7a503d30cb8d05e
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-12 13:14:12 +0800

    addfile:编码器模块，包括Kingkong编码器、AS5048编码器、Jerry自制编码器的驱动

---
+++ base/encoder/encoder.cpp
@@ -0,0 +1,82 @@
+/**
+******************************************************************************
+* @file    encoder.cpp/h
+* @brief   encoder communication(CAN). 编码器驱动板通信(CAN)
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
+
+#include "encoder.h"
+#include <string.h>
+
+KKEncoder::KKEncoder(CAN_HandleTypeDef* hcan) : hcan_(hcan), connect_(1000) {
+   connect_.check();
+}
+
+void KKEncoder::handle(void) {}
+
+// Check CAN channel and id of received CAN message
+// 校验接收信息的CAN通道和ID
+bool KKEncoder::canRxMsgCheck(CAN_HandleTypeDef* hcan,
+                             CAN_RxHeaderTypeDef rx_header) {
+   return hcan == hcan_ && (rx_header.StdId == id_);
+}
+
+// Receive feedback data message callback. Called in
+// HAL_CAN_RxFifo0MsgPendingCallback()
+// 信息接收回调，在HAL_CAN_RxFifo0MsgPendingCallback中调用
+void KKEncoder::canRxMsgCallback(CAN_HandleTypeDef* hcan,
+                                CAN_RxHeaderTypeDef rx_header,
+                                uint8_t rx_data[8]) {
+   memcpy(&rx_data_, rx_data, sizeof(rx_data_));
+   for (int i = 0; i < 3; i++) {
+       deg_[i] = (float)rx_data_.ecd[i] * 360.f / 65535.f;
+   }
+   connect_.refresh();
+}
+
+AS5048Encoder::AS5048Encoder(CAN_HandleTypeDef* hcan)
+   : hcan_(hcan), connect_(1000) {
+   connect_.check();
+}
+
+void AS5048Encoder::handle(void) {}
+
+// Check CAN channel and id of received CAN message
+// 校验接收信息的CAN通道和ID
+bool AS5048Encoder::canRxMsgCheck(CAN_HandleTypeDef* hcan,
+                                 CAN_RxHeaderTypeDef rx_header) {
+   return hcan == hcan_ && (rx_header.StdId >= 0x211 && rx_header.StdId <= 0x216);
+}
+
+// Receive feedback data message callback. Called in
+// HAL_CAN_RxFifo0MsgPendingCallback()
+// 信息接收回调，在HAL_CAN_RxFifo0MsgPendingCallback中调用
+void AS5048Encoder::canRxMsgCallback(CAN_HandleTypeDef* hcan,
+                                    CAN_RxHeaderTypeDef rx_header,
+                                    uint8_t rx_data[8]) {
+   memcpy(&rx_data_, rx_data, sizeof(rx_data_));
+   connect_.refresh();
+}
+
+JMEncoder::JMEncoder(CAN_HandleTypeDef* hcan, uint8_t id) : hcan_(hcan), connect_(50) {
+   connect_.check();
+   id_config(id);
+}
+
+void JMEncoder::handle(void) {}
+
+bool JMEncoder::canRxMsgCheck(CAN_HandleTypeDef* hcan,
+                             CAN_RxHeaderTypeDef rx_header) {
+   return hcan == hcan_ && (rx_header.StdId == id_);
+}
+
+void JMEncoder::canRxMsgCallback(CAN_HandleTypeDef* hcan,
+                                CAN_RxHeaderTypeDef rx_header,
+                                uint8_t* rx_data) {
+   memcpy(&rx_data_, rx_data, sizeof(rx_data_));
+   deg_ = (float)rx_data_.ecd_deg2 * 360.f / 65535.f;
+   connect_.refresh();
+}
---
+++ base/encoder/encoder.h
@@ -0,0 +1,117 @@
+/**
+******************************************************************************
+* @file    encoder.cpp/h
+* @brief   encoder communication(CAN). 编码器驱动板通信(CAN)
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
+
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include "common/connect/connect.h"
+#include "can.h"
+
+// Kingkong编码器
+class KKEncoder {
+public:
+   KKEncoder(CAN_HandleTypeDef* hcan);
+
+   void handle(void);
+
+   // Check CAN channel and id of received CAN message
+   // 校验接收信息的CAN通道和ID
+   bool canRxMsgCheck(CAN_HandleTypeDef* hcan, CAN_RxHeaderTypeDef rx_header);
+
+   // Receive feedback data message callback. Called in
+   // HAL_CAN_RxFifo0MsgPendingCallback()
+   // 信息接收回调，在HAL_CAN_RxFifo0MsgPendingCallback中调用
+   void canRxMsgCallback(CAN_HandleTypeDef* hcan, CAN_RxHeaderTypeDef rx_header,
+                         uint8_t rx_data[8]);
+
+public:
+   Connect connect_;
+
+   // 接收数据
+   struct RxData_t {
+       uint16_t ecd[3];
+       uint16_t reserve;
+   } rx_data_;
+
+   // 编码器角度
+   float deg_[3];
+
+private:
+   CAN_HandleTypeDef* hcan_;
+   const uint16_t id_ = 0x221;
+};
+
+// AS5048编码器
+class AS5048Encoder {
+public:
+   AS5048Encoder(CAN_HandleTypeDef* hcan);
+
+   void handle(void);
+
+   // Check CAN channel and id of received CAN message
+   // 校验接收信息的CAN通道和ID
+   bool canRxMsgCheck(CAN_HandleTypeDef* hcan, CAN_RxHeaderTypeDef rx_header);
+
+   // Receive feedback data message callback. Called in
+   // HAL_CAN_RxFifo0MsgPendingCallback()
+   // 信息接收回调，在HAL_CAN_RxFifo0MsgPendingCallback中调用
+   void canRxMsgCallback(CAN_HandleTypeDef* hcan, CAN_RxHeaderTypeDef rx_header,
+                         uint8_t rx_data[8]);
+
+public:
+   Connect connect_;
+
+   // 接收数据
+   struct RxData_t {
+       float deg;
+       uint8_t reserve[4];
+   } rx_data_;
+
+private:
+   CAN_HandleTypeDef* hcan_;
+};
+
+class JMEncoder {
+public:
+   JMEncoder(CAN_HandleTypeDef* hcan, uint8_t id);
+
+   void handle(void);
+
+   // Check CAN channel and id of received CAN message
+   // 校验接收信息的CAN通道和ID
+   bool canRxMsgCheck(CAN_HandleTypeDef* hcan, CAN_RxHeaderTypeDef rx_header);
+
+   // Receive feedback data message callback. Called in
+   // HAL_CAN_RxFifo0MsgPendingCallback()
+   // 信息接收回调，在HAL_CAN_RxFifo0MsgPendingCallback中调用
+   void canRxMsgCallback(CAN_HandleTypeDef* hcan, CAN_RxHeaderTypeDef rx_header,
+                         uint8_t* rx_data);
+
+   //确认can包ID
+   void id_config(uint8_t id) {id_ += id;}
+
+public:
+   Connect connect_;
+
+   // 接收数据
+   struct RxData_t {
+       float ecd_deg1;
+       uint16_t ecd_deg2;
+   } rx_data_;
+
+   // 编码器角度
+   float deg_;
+
+private:
+   CAN_HandleTypeDef* hcan_;
+   uint16_t id_ = 0x210;
+};
+
+#endif  // ENCODER_H
