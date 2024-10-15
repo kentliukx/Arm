313ae992ba500bc298e2961e2768e02c8a26a511
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-15 21:22:57 +0800

    add:在Chassis控制数据包中加入旋转跟随部分

--- common/message_center/msg_def.h
+++ common/message_center/msg_def.h
@@ -11,7 +11,9 @@ typedef struct {
     float vx; // x向速度
     float vy; // y向速度
     float wz; // 旋转角
-    float angle; // 底盘角度
+    float ref_angle; // 底盘角度
+
+    float fdb_angle, follow_fdb_angle;
     ChassisMode_e mode_; // 底盘模式
 } ChassisCtrlCmd;
 
