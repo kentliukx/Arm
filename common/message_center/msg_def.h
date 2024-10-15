8d14a24cc5d59228328d758faa8f890294c9b154
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-15 21:02:36 +0800

    add:添加chassis接收控制指令类型

---
+++ common/message_center/msg_def.h
@@ -0,0 +1,18 @@
+//
+// Created by 98383 on 24-10-15.
+//
+
+#ifndef RM_FRAME_MSG_DEF_H
+#define RM_FRAME_MSG_DEF_H
+
+#include "app/chassis/chassis.h"
+
+typedef struct {
+    float vx; // x向速度
+    float vy; // y向速度
+    float wz; // 旋转角
+    float angle; // 底盘角度
+    ChassisMode_e mode_; // 底盘模式
+} ChassisCtrlCmd;
+
+#endif//RM_FRAME_MSG_DEF_H
