708f49ee2bc2402733dc3b3cca0b670a7a1220a1
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-18 21:14:45 +0800

    addfile:功率限制

---
+++ common/power_limit/power_limit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 98383 on 24-10-18.
+//
+
+#include "power_limit.h"
---
+++ common/power_limit/power_limit.h
@@ -0,0 +1,8 @@
+//
+// Created by 98383 on 24-10-18.
+//
+
+#ifndef RM_FRAME_POWER_LIMIT_H
+#define RM_FRAME_POWER_LIMIT_H
+
+#endif  // RM_FRAME_POWER_LIMIT_H
