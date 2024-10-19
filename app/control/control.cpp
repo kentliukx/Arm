57d49ff9c9a32ff2ab6899bd67f26398ccaff49c
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-19 22:57:58 +0800

    addfile:添加control主控逻辑模块

---
+++ app/control/control.cpp
@@ -0,0 +1,12 @@
+/**
+******************************************************************************
+* @file    motor_monitor.cpp/h
+* @brief   Motor parameter, id config and management. 电机参数id配置和统一管理
+* @author  Likai Fu
+******************************************************************************
+* Copyright (c) 2025 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
+
+#include "control.h"
---
+++ app/control/control.h
@@ -0,0 +1,20 @@
+/**
+******************************************************************************
+* @file    motor_monitor.cpp/h
+* @brief   Motor parameter, id config and management. 电机参数id配置和统一管理
+* @author  Likai Fu
+******************************************************************************
+* Copyright (c) 2025 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
+
+#ifndef RM_FRAME_CONTROL_H
+#define RM_FRAME_CONTROL_H
+
+// 控制初始化
+void controlInit(void);
+// 控制主循环
+void controlLoop(void);
+
+#endif  // RM_FRAME_CONTROL_H
