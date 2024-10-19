a8f0a32a770e5e029d49c81f7d15f26e1b46715e
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-19 22:44:00 +0800

    add:添加motor_monitor电机管理

---
+++ base/monitor/motor_monitor.cpp
@@ -0,0 +1,155 @@
+/**
+******************************************************************************
+* @file    motor_monitor.cpp/h
+* @brief   Motor parameter, id config and management. 电机参数id配置和统一管理
+* @author  Spoon Guan
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
+
+#include "motor_monitor.h"
+
+#include "lib/arm_math/arm_math.h"
+
+// Motor parameter config
+// 电机参数配置
+
+Motor* can1_dji_motor[11] = {
+    nullptr,  // id:1
+    nullptr,  // id:2
+    nullptr,  // id:3
+    nullptr,  // id:4
+    nullptr,  // id:5
+    nullptr,  // id:6
+    nullptr,  // id:7
+    nullptr,  // id:8
+    nullptr,  // id:9
+    nullptr,  // id:10
+    nullptr   // id:11
+};
+Motor* can2_dji_motor[11] = {
+    nullptr,  // id:1
+    nullptr,  // id:2
+    nullptr,  // id:3
+    nullptr,  // id:4
+    nullptr,  // id:5
+    nullptr,  // id:6
+    nullptr,  // id:7
+    nullptr,  // id:8
+    nullptr,  // id:9
+    nullptr,  // id:10
+    nullptr   // id:11
+};
+
+DJIMotorDriver dji_motor_driver(can1_dji_motor, can2_dji_motor);
+
+// 电机停转速度上限(dps)
+const float motor_stop_rotate_speed_thres = 1200;
+
+// Stop and shut off all motors
+// 所有电机先停转再断电
+void allMotorsStopShutoff(void) {
+  for (int i = 0; i < 11; i++) {
+    if (can1_dji_motor[i] != nullptr &&
+        can1_dji_motor[i]->mode_ != Motor::INIT) {
+      if (fabs(can1_dji_motor[i]->motor_data_.rotate_speed) >
+          motor_stop_rotate_speed_thres) {
+        can1_dji_motor[i]->mode_ = Motor::STOP;
+      } else {
+        can1_dji_motor[i]->mode_ = Motor::POWEROFF;
+      }
+    }
+    if (can2_dji_motor[i] != nullptr &&
+        can2_dji_motor[i]->mode_ != Motor::INIT) {
+      if (fabs(can2_dji_motor[i]->motor_data_.rotate_speed) >
+          motor_stop_rotate_speed_thres) {
+        can2_dji_motor[i]->mode_ = Motor::STOP;
+      } else {
+        can2_dji_motor[i]->mode_ = Motor::POWEROFF;
+      }
+    }
+  }
+}
+
+// Shut off all motors
+// 所有电机断电
+void allMotorsShutOff(void) {
+  for (int i = 0; i < 11; i++) {
+    if (can1_dji_motor[i] != nullptr &&
+        can1_dji_motor[i]->mode_ != Motor::INIT) {
+      can1_dji_motor[i]->mode_ = Motor::POWEROFF;
+    }
+    if (can2_dji_motor[i] != nullptr &&
+        can2_dji_motor[i]->mode_ != Motor::INIT) {
+      can2_dji_motor[i]->mode_ = Motor::POWEROFF;
+    }
+  }
+}
+
+// Stop all motors
+// 所有电机停转
+void allMotorsStop(void) {
+  for (int i = 0; i < 11; i++) {
+    if (can1_dji_motor[i] != nullptr &&
+        can1_dji_motor[i]->mode_ != Motor::INIT) {
+      can1_dji_motor[i]->mode_ = Motor::STOP;
+    }
+    if (can2_dji_motor[i] != nullptr &&
+        can2_dji_motor[i]->mode_ != Motor::INIT) {
+      can2_dji_motor[i]->mode_ = Motor::STOP;
+    }
+  }
+}
+
+// Startup all motors
+// 所有电机启动
+void allMotorsOn(void) {
+  for (int i = 0; i < 11; i++) {
+    if (can1_dji_motor[i] != nullptr &&
+        can1_dji_motor[i]->mode_ != Motor::INIT) {
+      can1_dji_motor[i]->mode_ = Motor::WORKING;
+    }
+    if (can2_dji_motor[i] != nullptr &&
+        can2_dji_motor[i]->mode_ != Motor::INIT) {
+      can2_dji_motor[i]->mode_ = Motor::WORKING;
+    }
+  }
+}
+
+// Reset all motors.
+// 电机统一初始化
+void allMotorsInit(void) {
+  dji_motor_driver.idConfig();
+  for (int i = 0; i < 11; i++) {
+    if (can1_dji_motor[i] != nullptr) {
+      can1_dji_motor[i]->reset();
+    }
+    if (can2_dji_motor[i] != nullptr) {
+      can2_dji_motor[i]->reset();
+    }
+  }
+}
+
+// Handle all motors. Called in motorTask
+// 电机统一处理，在motorTask中调用
+void allMotorsHandle(void) {
+  for (int i = 0; i < 11; i++) {
+    if (can1_dji_motor[i] != nullptr) {
+      can1_dji_motor[i]->handle();
+    }
+    if (can2_dji_motor[i] != nullptr) {
+      can2_dji_motor[i]->handle();
+    }
+  }
+}
+
+// Check CAN channel and id of received CAN message
+// 校验接收信息的CAN通道和ID，调用对应回调函数
+void motorsCanRxMsgHandle(CAN_HandleTypeDef* hcan,
+                          CAN_RxHeaderTypeDef rx_header, uint8_t* rx_data) {
+  if (dji_motor_driver.canRxMsgCheck(hcan, rx_header)) {
+    dji_motor_driver.canRxMsgCallback(hcan, rx_header, rx_data);
+  }
+}
---
+++ base/monitor/motor_monitor.h
@@ -0,0 +1,43 @@
+/**
+******************************************************************************
+* @file    motor_monitor.cpp/h
+* @brief   Motor parameter, id config and management. 电机参数id配置和统一管理
+* @author  Spoon Guan
+******************************************************************************
+* Copyright (c) 2023 Team JiaoLong-SJTU
+* All rights reserved.
+******************************************************************************
+*/
+
+#ifndef MOTOR_MONITOR_H
+#define MOTOR_MONITOR_H
+
+#include "base/motor/motor.h"
+#include "base/motor/motor_driver/dji_motor_driver.h"
+
+// Manage all motors' mode. Called in controlTask.
+// 统一操作电机模式，在controlTask中调用
+void allMotorsStopShutoff(void);
+void allMotorsShutOff(void);
+void allMotorsStop(void);
+void allMotorsOn(void);
+
+// Reset all motors.
+// 电机统一初始化
+void allMotorsInit(void);
+
+// Handle all motors. Called in motorTask
+// 电机统一处理，在motorTask中调用
+void allMotorsHandle(void);
+
+// Check CAN channel and id of received CAN message
+// 校验接收信息的CAN通道和ID，调用对应回调函数
+void motorsCanRxMsgHandle(CAN_HandleTypeDef* hcan,
+                          CAN_RxHeaderTypeDef rx_header, uint8_t* rx_data);
+
+// extern Motor CMFL, CMFR, CMBL, CMBR, GMY, GMP, FRICL, FRICR, STIR, m1;
+// extern Motor STFL, STFR, STBL, STBR;
+// extern Motor* can1_dji_motor[11];
+// extern Motor* can2_dji_motor[11];
+
+#endif  // MOTOR_MONITOR_H
