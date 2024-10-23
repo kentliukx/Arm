717fa2dd85a4e4cb36cd65cb2a955cfe9fda665f
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-23 19:29:28 +0800

    add:声明航向电机及其电机驱动

--- base/monitor/motor_monitor.h
+++ base/monitor/motor_monitor.h
@@ -36,9 +36,8 @@ void motorsCanRxMsgHandle(CAN_HandleTypeDef* hcan,
                           CAN_RxHeaderTypeDef rx_header, uint8_t* rx_data);
 
 extern Motor CMFL, CMFR, CMBL, CMBR;
-
+extern Motor STFL, STFR, STBL, STBR;
 // extern Motor CMFL, CMFR, CMBL, CMBR, GMY, GMP, FRICL, FRICR, STIR, m1;
-// extern Motor STFL, STFR, STBL, STBR;
 // extern Motor* can1_dji_motor[11];
 // extern Motor* can2_dji_motor[11];
 
