054de287c48b9420c7a501701412d4212dd84696
LikaiF <56214912+RoyalFly@users.noreply.github.com>
2024-10-25 22:17:28 +0800

    addfile:gimbal类文件

---
+++ app/gimbal/gimbal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 98383 on 24-10-25.
+//
+
+#include "gimbal.h"
---
+++ app/gimbal/gimbal.h
@@ -0,0 +1,14 @@
+//
+// Created by 98383 on 24-10-25.
+//
+
+#ifndef RM_FRAME_GIMBAL_H
+#define RM_FRAME_GIMBAL_H
+
+class Gimbal {
+ public:
+  virtual void init(void);
+  void SetAngleSpeed();
+};
+
+#endif  // RM_FRAME_GIMBAL_H
